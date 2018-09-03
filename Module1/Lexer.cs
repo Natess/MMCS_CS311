f591b8039dfe600f8bc46c5d50ee23aa21cde47b
Natess <kuzanastyav@gmail.com>
2018-09-03 13:25:46 +0300

    Add files via upload

--- Module1/Lexer.cs
+++ Module1/Lexer.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+
 public class LexerException : System.Exception
 {
     public LexerException(string msg)
@@ -57,14 +60,20 @@ public class IntLexer : Lexer
 
     public override void Parse()
     {
+        var result = 0;
+        var sign_p = true;
+
         NextCh();
         if (currentCh == '+' || currentCh == '-')
         {
+            if (currentCh == '-')
+                sign_p = false;
             NextCh();
         }
 
         if (char.IsDigit(currentCh))
         {
+            result += int.Parse(currentCh.ToString()) ;
             NextCh();
         }
         else
@@ -74,6 +83,8 @@ public class IntLexer : Lexer
 
         while (char.IsDigit(currentCh))
         {
+            result *= 10;
+            result += int.Parse(currentCh.ToString());
             NextCh();
         }
 
@@ -83,26 +94,143 @@ public class IntLexer : Lexer
             Error();
         }
 
+        if (!sign_p)
+            result *= -1;
         System.Console.WriteLine("Integer is recognized");
-
+        Console.WriteLine("The number is {0}.", result);
+        //return result;
     }
 }
 
+public class IdentifierLexer: Lexer
+{
+    protected System.Text.StringBuilder identifierString;
+
+    public IdentifierLexer(string input) : base(input)
+    {
+        identifierString = new System.Text.StringBuilder();
+
+    }
+    
+    public override void Parse()
+    {
+        var result = "";
+        NextCh();
+        if (char.IsLetter(currentCh))
+        {
+            result += currentCh;
+            NextCh();
+        }
+        else
+            Error();
+
+        while (char.IsDigit(currentCh) || char.IsLetter(currentCh) || currentCh == '_')
+        {
+            result += currentCh;
+            NextCh();
+        } 
+
+        if (currentCharValue != -1) // StringReader вернет -1 в конце строки
+            Error();
+        System.Console.WriteLine("Identifier {0} is recognized", result);
+    }
+}
+
+public class NonZeroInStartIntegerLexer: Lexer
+{
+    protected System.Text.StringBuilder resString;
+
+    public NonZeroInStartIntegerLexer(string input)
+        : base(input)
+    {
+        resString = new System.Text.StringBuilder();
+    }
+
+    public override void Parse()
+    { 
+        NextCh();
+        if (currentCh == '+' || currentCh == '-')
+        {
+            resString.Append(currentCh);
+            NextCh();
+        }
+
+        if (char.IsDigit(currentCh) && int.Parse(currentCh.ToString()) != 0)
+        {
+            resString.Append(currentCh);
+            NextCh();
+        }
+        else
+        {
+            Error();
+        }
+
+        while (char.IsDigit(currentCh))
+        {
+            resString.Append(currentCh);
+            NextCh();
+        }
+
+
+        if (currentCharValue != -1) // StringReader вернет -1 в конце строки
+            Error();
+
+        System.Console.WriteLine("String {0} is recognized", resString);
+    }
+}
+
+//public class StripedLexerStartingWithLetter: Lexer
+//{
+    
+//}
 
 public class Program
 {
     public static void Main()
     {
-        string input = "154216";
-        Lexer L = new IntLexer(input);
-        try
+        Console.WriteLine("\nIntLexer\n");
+        List<string> input = new List<string>{ "154216" ,  "-9908", "88al",  "08", "", "1", "8+"} ;
+        foreach(var i in input)
         {
-            L.Parse();
-        }
-        catch (LexerException e)
-        {
-            System.Console.WriteLine(e.Message);
+            Lexer L = new IntLexer(i);
+            try
+            {
+                L.Parse();
+            }
+            catch (LexerException e)
+            {
+                System.Console.WriteLine(e.Message);
+            }
         }
 
+        Console.WriteLine("\nIdentifierLexer\n");
+        input = new List<string> { "", "1", "9a", "aaaa2", "A87d787a", "aa7J", "_", "1_", "a_", "a9_", "as_d_8_j_7_8", "+h8", "hh-" };
+        foreach (var i in input)
+        {
+            Lexer L = new IdentifierLexer(i);
+            try
+            {
+                L.Parse();
+            }
+            catch (LexerException e)
+            {
+                System.Console.WriteLine(e.Message);
+            }
+        }
+
+        Console.WriteLine("\nNonZeroInStartIntegerLexer\n");
+        input = new List<string> { "", "1", "9a", "aaaa2",  "_", "1_", "+h8", "-9", "0", "09", "00", "70", "-08" };
+        foreach (var i in input)
+        {
+            Lexer L = new NonZeroInStartIntegerLexer(i);
+            try
+            {
+                L.Parse();
+            }
+            catch (LexerException e)
+            {
+                System.Console.WriteLine(e.Message);
+            }
+        }
     }
 }
